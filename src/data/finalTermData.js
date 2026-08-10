eebd8d94d4cd0f275f8d84cd69c39b4f28e512ab
Sahirullah <sahirullah313@gmail.com>
2026-08-10 09:32:13 +0500

    updaoitng

--- src/data/finalTermData.js
+++ src/data/finalTermData.js
@@ -47,10 +47,6 @@ export const finalTermData = [
     'https://docs.google.com/document/d/1xCO3RVOjj6RCMm14SpQeanGUWLcNqD-E/edit?usp=sharing&ouid=114958120571553419825&rtpof=true&sd=true',
     'https://drive.google.com/file/d/1SLNrM24LyzekBLMoLNMfCjVoLSbhdNry/view?usp=sharing',
     'https://docs.google.com/document/d/1MeTZB1RawaHHzUewQ5OyQWIkXiZwPl0D/edit?usp=sharing&ouid=114958120571553419825&rtpof=true&sd=true',
-    'https://drive.google.com/file/d/1vyR2VH8sGDoqIaoeiL58Fw-WUqLJIcER/view?usp=sharing',
-    'https://drive.google.com/file/d/1e9UvVAD-yC2o-AfMkkw2QmtYh88m2em8/view?usp=sharing',
-    'https://drive.google.com/file/d/1ymBHIG8dmpydj4R0cd6ae83Boz9p3c0T/view?usp=sharing',
-    'https://drive.google.com/file/d/19ZsO3vSEIFotW3iQz5jbWnXNTl-Nms7t/view?usp=sharing',
     'https://drive.google.com/file/d/18KMBuxL47LVSO0PEgLFDoE3jKrhbjrhI/view?usp=sharing',
     'https://drive.google.com/file/d/1AaOhNEhFieTX34D4OAIRW1NCpFCTqScF/view?usp=sharing'
   ]},
