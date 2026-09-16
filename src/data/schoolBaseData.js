9c0d4859d6d49b436f56041906d141415816d334
Sahirullah <sahirullah313@gmail.com>
2026-09-16 10:12:31 +0500

    updating

--- src/data/schoolBaseData.js
+++ src/data/schoolBaseData.js
@@ -126,11 +126,11 @@ export const schoolBaseData = [
   { id: 33, code: 'BOARD018', title: 'BISE Multan', category: 'boards', image: '📚', link: '' },
 //        >>>>------<<<<<       KOHAT TEXTBOOK BOARD       >>>>------<<<<<
   { id: 34, code: 'PTBB', title: 'BISE Peshawar 1st class Drawing books', category: 'boards', image: '📚', link: 'https://drive.google.com/file/d/1pw9hfX3e5WNfs2Yi9_yEb4QcCiToTq7X/view?usp=sharing' },
-  { id: 34, code: 'PTBB', title: 'BISE Peshawar', category: 'boards', image: '📚', link: '' },
-  { id: 34, code: 'PTBB', title: 'BISE Peshawar', category: 'boards', image: '📚', link: '' },
-  { id: 34, code: 'PTBB', title: 'BISE Peshawar', category: 'boards', image: '📚', link: '' },
-  { id: 34, code: 'BOARD019', title: 'BISE Peshawar', category: 'boards', image: '📚', link: '' },
-  { id: 34, code: 'BOARD019', title: 'BISE Peshawar', category: 'boards', image: '📚', link: '' },
+  { id: 34, code: 'PTBB', title: 'BISE Peshawar 1st class', category: 'boards', image: '📚', link: '' },
+  { id: 34, code: 'PTBB', title: 'BISE Peshawar 1st class', category: 'boards', image: '📚', link: '' },
+  { id: 34, code: 'PTBB', title: 'BISE Peshawar 1st class', category: 'boards', image: '📚', link: '' },
+  { id: 34, code: 'PTBB', title: 'BISE Peshawar 1st class', category: 'boards', image: '📚', link: '' },
+  { id: 34, code: 'PTBB', title: 'BISE Peshawar 1st class', category: 'boards', image: '📚', link: '' },
 
   { id: 35, code: 'BOARD020', title: 'BISE Quetta', category: 'boards', image: '📚', link: '' },
   { id: 36, code: 'BOARD021', title: 'BISE Rawalpindi', category: 'boards', image: '📚', link: '' },
