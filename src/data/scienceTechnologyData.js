8712326e2262bf78fe46eb326023422192ae3183
Sahirullah <sahirullah313@gmail.com>
2026-09-16 12:52:00 +0500

    updating

--- src/data/scienceTechnologyData.js
+++ src/data/scienceTechnologyData.js
@@ -6,6 +6,7 @@ export const scienceTechnologyData = [
   { id: 4, code: 'CS201', title: 'Introduction to Programming', category: 'cs', image: '💻', link: '' },
   { id: 5, code: 'BIO101', title: 'Biology I', category: 'bio', image: '🧪', link: '' },
   { id: 6, code: 'MAT201', title: 'Advanced Mathematics', category: 'mat', image: '📐', link: '' },
+  { id: 6, code: 'Engineering', title: 'Civil Engineering', category: 'engineering', image: '📐', link: 'https://drive.google.com/file/d/1Z_H42IcJeGYprfLYoXn5L12l8NHFFW9i/view?usp=sharing' },
 ];
 
 export const scienceTechnologyCategories = [
@@ -15,4 +16,5 @@ export const scienceTechnologyCategories = [
   { id: 'che', name: 'Chemistry' },
   { id: 'bio', name: 'Biology' },
   { id: 'mat', name: 'Mathematics' },
+  { id: 'engineering', name: 'Engineering' },
 ];
