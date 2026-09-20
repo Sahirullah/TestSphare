bade5662f65be3aaf81e280fa0fcfb1d91a758f2
Sahirullah <sahirullah313@gmail.com>
2026-09-20 10:00:10 +0500

    Add missing literatureLanguagesData file to fix build error

---
+++ src/data/literatureLanguagesData.js
@@ -0,0 +1,51 @@
+export const literatureLanguagesData = [
+  {
+    id: 1,
+    code: 'LIT-001',
+    title: 'English Literature',
+    image: '📚',
+    link: '#'
+  },
+  {
+    id: 2,
+    code: 'LIT-002',
+    title: 'Urdu Literature',
+    image: '📖',
+    link: '#'
+  },
+  {
+    id: 3,
+    code: 'LAN-001',
+    title: 'English Grammar',
+    image: '✍️',
+    link: '#'
+  },
+  {
+    id: 4,
+    code: 'LAN-002',
+    title: 'Arabic Language',
+    image: '📝',
+    link: '#'
+  },
+  {
+    id: 5,
+    code: 'LAN-003',
+    title: 'French Language',
+    image: '🗣️',
+    link: '#'
+  },
+  {
+    id: 6,
+    code: 'LIT-003',
+    title: 'World Literature',
+    image: '🌍',
+    link: '#'
+  }
+];
+
+export const literatureLanguagesCategories = [
+  { id: 'all', name: 'All Materials' },
+  { id: 'literature', name: 'Literature' },
+  { id: 'languages', name: 'Languages' },
+  { id: 'grammar', name: 'Grammar' }
+];
