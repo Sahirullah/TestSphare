4f26dca4c65581ed21cb52b0678135d0b5a97a86
Sahirullah <sahirullah313@gmail.com>
2026-09-20 08:12:36 +0500

    work in the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -136,9 +136,9 @@ const Header = () => {
               </button>
               <ul className={`vu-dropdown-menu ${isVUFilesOpen ? 'vu-dropdown-open' : ''}`}>
                 <li><Link to="/vu-handout" onClick={() => { setIsVUMenuOpen(false); setIsVUFilesOpen(false); }}>All Books</Link></li>
-                <li><Link to="/highlighted-handout" onClick={() => { setIsVUMenuOpen(false); setIsVUFilesOpen(false); }}>School Base</Link></li>
-                <li><Link to="/midterm" onClick={() => { setIsVUMenuOpen(false); setIsVUFilesOpen(false); }}>Past Papers</Link></li>
                 <li><Link to="/final-term" onClick={() => { setIsVUMenuOpen(false); setIsVUFilesOpen(false); }}>Files Hub</Link></li>
+                <li><Link to="/midterm" onClick={() => { setIsVUMenuOpen(false); setIsVUFilesOpen(false); }}>Past Papers</Link></li>
+                <li><Link to="/highlighted-handout" onClick={() => { setIsVUMenuOpen(false); setIsVUFilesOpen(false); }}>School Base</Link></li>
                 <li><Link to="/uni-base" onClick={() => { setIsVUMenuOpen(false); setIsVUFilesOpen(false); }}>University Base</Link></li>
                 <li><Link to="/assignment" onClick={() => { setIsVUMenuOpen(false); setIsVUFilesOpen(false); }}>Assignments</Link></li>
                 <li><a href="#gdbs" onClick={() => { setIsVUMenuOpen(false); setIsVUFilesOpen(false); }}>GDBs</a></li>
