d7fd7bc6a1e5fb11191982787f91adf15eeea446
Sahirullah <sahirullah313@gmail.com>
2026-09-20 10:43:58 +0500

    linking

--- src/data/hecPastPapersData.js
+++ src/data/hecPastPapersData.js
@@ -2,22 +2,22 @@ export const hecPastPapersData = [
   //HEC NOTES
   { id: 1, code: 'HEC-NOTES', title: 'HEC Basic Geometric Formulas & Properties', image: '📄', category: 'notes', link: 'https://drive.google.com/file/d/1VOSMzB5h-ck4cnpP-E2KrLgD7rljK7Nd/view?usp=sharing' },
   { id: 2, code: 'HEC-NOTES', title: 'HEC General knowledge Notes', image: '📄', category: 'notes', link: 'https://drive.google.com/file/d/1PuCAqMa6QyNWyclP9eghwaX5CSdpHrrA/view?usp=sharing' },
-  { id: 2, code: 'HEC-NOTES', title: 'HEC General knowledge Notes', image: '📄', category: 'notes', link: 'https://drive.google.com/file/d/1zVP0fBvh8XBfj6pphOyZDlcnmzAVGTMk/view?usp=sharing' },
-  { id: 2, code: 'HEC-NOTES', title: 'HEC General knowledge Notes', image: '📄', category: 'notes', link: '' },
+  { id: 3, code: 'HEC-NOTES', title: 'HEC General knowledge Notes', image: '📄', category: 'notes', link: 'https://drive.google.com/file/d/1zVP0fBvh8XBfj6pphOyZDlcnmzAVGTMk/view?usp=sharing' },
+  { id: 4, code: 'HEC-NOTES', title: 'HEC General knowledge Notes', image: '📄', category: 'notes', link: '' },
 
   //HEC past paper
-  { id: 3, code: 'HEC-Past paper', title: 'HEC Past Papers 2022/2023', image: '📄', category: 'paper', link: 'https://drive.google.com/file/d/1Ovq6JVGOJBif3He5oYNBzMORYvgN6gfo/view?usp=sharing' },
-  { id: 4, code: 'HEC-Past paper', title: 'HEC Past Papers 2019 Answer key', image: '📄', category: 'paper', link: 'https://drive.google.com/file/d/13pORr0Q_w_JMGnF-otcmvGAGmoCM3CBL/view?usp=sharing' },
-  { id: 5, code: 'HEC-Past paper', title: 'HEC Past Papers pdf', image: '📄', category: 'paper', link: 'https://drive.google.com/file/d/1fnX7rv1p2UmEEJBjmu3ClxTgcNC51w-w/view?usp=sharing' },
-  { id: 6, code: 'Past paper', title: 'HEC Past Papers 2017 pdf', image: '📄', category: 'paper', link: 'https://drive.google.com/file/d/18zCFoJvIwfUOQjbMOa9GXXENO-cGeabi/view?usp=sharing' },
-  { id: 7, code: 'Past paper', title: 'HEC Past Papers 2019 pdf', image: '📄', category: 'paper', link: 'https://drive.google.com/file/d/1Vwr-ZITt6MZRsowfP96htwdVlmXhrQb4/view?usp=sharing' },
-  { id: 8, code: 'Past paper', title: 'HEC Past Papers 2022/2023', image: '📄', category: 'paper', link: 'https://drive.google.com/file/d/1TIaUYlyOKJrwOQAZT2pBTR5GmZEkDEWo/view?usp=sharing' },
-  { id: 9, code: 'Past paper', title: 'HEC Past Papers 2022/2023 pdf', image: '📄', category: 'paper', link: 'https://drive.google.com/file/d/1tGJeoTp77qkym5jI0pFAZ8YRf9bjN5V2/view?usp=sharing' },
-  { id: 10, code: 'Past paper', title: 'HEC Past Papers 2024 pdf', image: '📄', category: 'paper', link: 'https://drive.google.com/file/d/19ylhYjFa2EFAQYlIJjD6OPuXK6vBxzcl/view?usp=sharing' },
-  { id: 11, code: 'Past paper', title: 'HEC Past Papers 2018 pdf', image: '📄', category: 'paper', link: 'https://drive.google.com/file/d/12W4JZIQb9VeTDv8cI7YrFHiNlhULOy9l/view?usp=sharing' },
-  { id: 12, code: 'Past paper', title: 'HEC phase1 Past Paper with answers key by Faraz malik 2022/2023', image: '📄', category: 'paper', link: 'https://drive.google.com/file/d/1mjPf4HrdG2TZGT9t7useMgYtxzQLnOdE/view?usp=sharing' },
-  { id: 13, code: 'Past paper', title: 'HEC Past Papers 2023/2024 pdf', image: '📄', category: 'paper', link: 'https://drive.google.com/file/d/1bQIhOnOD1aXESTz6QUpOn2HWmRK9Vd-E/view?usp=sharing' },
-  { id: 14, code: 'Past paper', title: 'HEC Past Papers 2023/2024 pdf', image: '📄', category: 'paper', link: 'https://drive.google.com/file/d/1bQIhOnOD1aXESTz6QUpOn2HWmRK9Vd-E/view?usp=sharing' }
+  { id: 5, code: 'HEC-Past paper', title: 'HEC Past Papers 2022/2023', image: '📄', category: 'paper', link: 'https://drive.google.com/file/d/1Ovq6JVGOJBif3He5oYNBzMORYvgN6gfo/view?usp=sharing' },
+  { id: 6, code: 'HEC-Past paper', title: 'HEC Past Papers 2019 Answer key', image: '📄', category: 'paper', link: 'https://drive.google.com/file/d/13pORr0Q_w_JMGnF-otcmvGAGmoCM3CBL/view?usp=sharing' },
+  { id: 7, code: 'HEC-Past paper', title: 'HEC Past Papers pdf', image: '📄', category: 'paper', link: 'https://drive.google.com/file/d/1fnX7rv1p2UmEEJBjmu3ClxTgcNC51w-w/view?usp=sharing' },
+  { id: 8, code: 'Past paper', title: 'HEC Past Papers 2017 pdf', image: '📄', category: 'paper', link: 'https://drive.google.com/file/d/18zCFoJvIwfUOQjbMOa9GXXENO-cGeabi/view?usp=sharing' },
+  { id: 9, code: 'Past paper', title: 'HEC Past Papers 2019 pdf', image: '📄', category: 'paper', link: 'https://drive.google.com/file/d/1Vwr-ZITt6MZRsowfP96htwdVlmXhrQb4/view?usp=sharing' },
+  { id: 10, code: 'Past paper', title: 'HEC Past Papers 2022/2023', image: '📄', category: 'paper', link: 'https://drive.google.com/file/d/1TIaUYlyOKJrwOQAZT2pBTR5GmZEkDEWo/view?usp=sharing' },
+  { id: 11, code: 'Past paper', title: 'HEC Past Papers 2022/2023 pdf', image: '📄', category: 'paper', link: 'https://drive.google.com/file/d/1tGJeoTp77qkym5jI0pFAZ8YRf9bjN5V2/view?usp=sharing' },
+  { id: 12, code: 'Past paper', title: 'HEC Past Papers 2024 pdf', image: '📄', category: 'paper', link: 'https://drive.google.com/file/d/19ylhYjFa2EFAQYlIJjD6OPuXK6vBxzcl/view?usp=sharing' },
+  { id: 13, code: 'Past paper', title: 'HEC Past Papers 2018 pdf', image: '📄', category: 'paper', link: 'https://drive.google.com/file/d/12W4JZIQb9VeTDv8cI7YrFHiNlhULOy9l/view?usp=sharing' },
+  { id: 14, code: 'Past paper', title: 'HEC phase1 Past Paper with answers key by Faraz malik 2022/2023', image: '📄', category: 'paper', link: 'https://drive.google.com/file/d/1mjPf4HrdG2TZGT9t7useMgYtxzQLnOdE/view?usp=sharing' },
+  { id: 15, code: 'Past paper', title: 'HEC Past Papers 2023/2024 pdf', image: '📄', category: 'paper', link: 'https://drive.google.com/file/d/1bQIhOnOD1aXESTz6QUpOn2HWmRK9Vd-E/view?usp=sharing' },
+  { id: 16, code: 'Past paper', title: 'HEC Past Papers 2023/2024 pdf', image: '📄', category: 'paper', link: 'https://drive.google.com/file/d/1bQIhOnOD1aXESTz6QUpOn2HWmRK9Vd-E/view?usp=sharing' }
 ];
 
 export const hecPastPapersCategories = [
